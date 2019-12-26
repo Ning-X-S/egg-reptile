49d2eee66487b6dafc92a0810e0f51bc9f2d0830
qiuwenju <540421017@qq.com>
2019-12-26 11:06:09 +0800

    ADD:列表查询实现分页

--- app/service/home.js
+++ app/service/home.js
@@ -8,23 +8,45 @@ class HomeService extends Service {
     const { ctx } = this;
     let data = {};
     try {
-      const list = await ctx.model.ReptileModel.findAll();
-
+      const { page = 1, size: limit = 10 } = ctx.query;
+      console.log(page);
+      console.log(limit);
+      const queryObj = {
+        attributes: [ 'id', 'title' ],
+        offset: parseInt(limit) * (page - 1),
+        limit: parseInt(limit),
+        where: {
+          id: 28,
+        },
+      };
+      const list = await ctx.model.ReptileModel.findAll(queryObj);
+      console.log(ctx.request.body);
+      console.log(ctx.query);
+      // 当前文件夹的目录
+      console.log(__dirname);
+      // 当前文件的目录
+      console.log(__filename);
+      // 进程运行中的目录
+      console.log(process.cwd());
+      const newsList = [];
+      list.forEach(item => {
+        newsList.push(item.get());
+      });
       data = {
         error_code: 0,
         data: {
-          list,
+          list: newsList,
+          total: list.count,
         },
         message: '成功',
       };
     } catch (err) {
       data = {
-        error_code: 0,
+        error_code: 40000001,
         data: {},
-        message: '失败',
+        message: '查询失败',
       };
     }
-    console.log(data);
     return data;
   }
 }
